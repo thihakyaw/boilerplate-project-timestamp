49370424980e1deb85f8a2bcbb129b4be1419694
Thiha Kyaw <thihakyaw.dev@gmail.com>
2021-07-06 00:23:41 +0630

    added time parser

--- server.js
+++ server.js
@@ -20,13 +20,19 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let parsedDate = new Date(req.params.date * 1000)
+  console.log(parsedDate.toGMTString())
+
+  if(parsedDate == 'Invalid Date') {
+    res.send({error : "Invalid Date"})
+  }
+
+  res.send({unix:req.params.date, utc: parsedDate.toGMTString()});
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
